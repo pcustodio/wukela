bd85621e7ef632507bb5cafd5d26741408a4887b
Paulo Custódio <paulo.custodio@gmail.com>
2020-03-27 18:47:58 +0000

    customize Kingfisher (size, placeholder) and tweak uitableview

--- wukela/Controller/HomeViewController.swift
+++ wukela/Controller/HomeViewController.swift
@@ -15,7 +15,6 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     let data = NewsLoader().news
-//    var newsURL : String = ""
     var picURL : String = ""
     
     override func viewDidLoad() {
@@ -24,15 +23,23 @@ class HomeViewController: UIViewController {
         //bkg color
         view.backgroundColor = UIColor(named: "bkColor")
         
-//        //remove extraneous empty cells
-//        tableView.tableFooterView = UIView()
+        //remove extraneous empty cells
+        tableView.tableFooterView = UIView()
+        
+        //hide separator line
+        self.tableView.separatorColor = .clear;
         
         //trigger UITableViewDataSource
         tableView.dataSource = self
         
         //trigger UITableViewDelegate
         tableView.delegate = self
+
+        //set cell height
+        self.tableView.rowHeight = 70;
+
     }
+    
 }
 
 //MARK: - TableView
@@ -48,7 +55,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     //create our cell
     //indexpath indicates which cell to display on each TableView row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
         let newsRow: NewsData
         newsRow = data[indexPath.row]
@@ -57,9 +64,12 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
         cell.detailTextLabel?.text = newsRow.news_src
         
         let url = URL(string: newsRow.img_src)!
+        let image = UIImage(named: "placeholder.pdf")
         cell.imageView?.kf.indicatorType = .activity
-        let processor = RoundCornerImageProcessor(cornerRadius: 15)
-        cell.imageView?.kf.setImage(with: url, options: [.processor(processor), .transition(.fade(0.2))], completionHandler: { (image, error, cacheType, URL) in
+    
+        let processor = DownsamplingImageProcessor(size: CGSize(width: 100, height: 100)) |> CroppingImageProcessor(size: CGSize(width: 50, height: 50), anchor: CGPoint(x: 0, y: 0)) |> RoundCornerImageProcessor(cornerRadius: 5)
+        
+        cell.imageView?.kf.setImage(with: url, placeholder: image, options: [.processor(processor), .transition(.fade(0.5))], completionHandler: { (image, error, cacheType, URL) in
             cell.setNeedsLayout()
         })
         
@@ -77,6 +87,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "getNews" {
