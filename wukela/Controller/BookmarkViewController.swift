445d7aa76f69cb79d7b537c52ecf6b9b52c2d25b
Paulo Custódio <paulo.custodio@gmail.com>
2020-03-29 17:35:08 +0100

    add Bookmarks tableview

--- wukela/Controller/BookmarkViewController.swift
+++ wukela/Controller/BookmarkViewController.swift
@@ -11,6 +11,8 @@ import CoreData
 
 class BookmarkViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,33 +20,50 @@ class BookmarkViewController: UIViewController {
         //bkg color
         view.backgroundColor = UIColor(named: "bkColor")
         
-        retrieveData()
+        //trigger UITableViewDataSource
+        tableView.dataSource = self
+        
+        //trigger UITableViewDelegate
+        tableView.delegate = self
+        
+        //hide separator line
+        self.tableView.separatorColor = .clear;
+        
+        //set cell height
+        self.tableView.rowHeight = 80;
+
 
     }
+}
     
-    //MARK: - Retrieve CoreData
-    
-    func retrieveData() {
-        
-        print("retrieving data")
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Bookmarks")
+//MARK: - TableView
 
-        do {
-            let result = try managedContext.fetch(fetchRequest)
-            
-            //Loop over CoreData entities
-            for data in result as! [NSManagedObject] {
-                
-                print(data.value(forKeyPath: "headlineMarked") as! String)
+extension BookmarkViewController: UITableViewDataSource, UITableViewDelegate {
 
-    
-            }
-        } catch {
-            print("Failed")
-        }
+    //how many rows on TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return nr of messages dynamically
+        return 1
     }
     
+    //create our cell
+    //indexpath indicates which cell to display on each TableView row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        
+        return cell
+        
+    }
+    
+    //cell was tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        //will print cell that was tapped on
+        //print(indexPath.row)
+
+        //deselect row
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 }
