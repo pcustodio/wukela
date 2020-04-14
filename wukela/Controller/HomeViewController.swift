5fbae9948dbad016ba6bc21b5e2685c9fa564b7b
Paulo Custódio <paulo.custodio@gmail.com>
2020-04-14 19:12:49 +0100

    removed DispatchQueue.main.async from newsLoader

--- wukela/Controller/HomeViewController.swift
+++ wukela/Controller/HomeViewController.swift
@@ -106,14 +106,12 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     func newsRefresh() {
         if Reachability.isConnectedToNetwork(){
-            DispatchQueue.main.async{
-                if self.segmentControl.selectedSegmentIndex == 0 {
-                    self.filteredData = NewsLoader().filterNews
-                } else {
-                    self.data = NewsLoader().news
-                }
-                self.tableView.reloadData()
+            if self.segmentControl.selectedSegmentIndex == 0 {
+                self.filteredData = NewsLoader().filterNews
+            } else {
+                self.data = NewsLoader().news
             }
+            self.tableView.reloadData()
         } else {
             print("Internet Connection not Available!")
             let alert = UIAlertController(title: "Connection Error", message: "Please check if your internet connection is active.", preferredStyle: .alert)
