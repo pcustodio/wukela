1f1dc471d10f6d963e52bf824f6f9efb24356b6d
Paulo Custódio <paulo.custodio@gmail.com>
2020-06-30 16:49:01 +0100

    remove onboarding collection view, adjust localisation

--- wukela/Controller/OnboardingViewController.swift
+++ wukela/Controller/OnboardingViewController.swift
@@ -9,13 +9,10 @@
 import UIKit
 import CoreData
 
-class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class OnboardingViewController: UIViewController {
 
     @IBOutlet weak var headliner: UILabel!
     @IBOutlet weak var subliner: UILabel!
-    
-    
-    let sourceImages: [UIImage] = [UIImage(named: "sourceImages01")!, UIImage(named: "sourceImages02")!, UIImage(named: "sourceImages03")!, UIImage(named: "sourceImages04")!, UIImage(named: "sourceImages05")!, UIImage(named: "sourceImages06")! ]
     
     let sources = ["Daily Nation",
                    "Punch",
@@ -29,7 +26,6 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var subLabel: UILabel!
     @IBOutlet weak var nextBtn: UIButton!
-    @IBOutlet weak var collectionView: UICollectionView!
     
     var currentlySelected = 0
 
@@ -39,11 +35,6 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
         //set text
         headliner.text = NSLocalizedString("Headliner", comment: "")
         subliner.text = NSLocalizedString("Subliner", comment: "")
-        
-        //config collection view
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView?.allowsMultipleSelection = true
         
         //set button
         nextBtn.isEnabled = false
@@ -86,7 +77,7 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
                 mainSyncLabel.center = CGPoint(x: actInd.center.x, y: actInd.center.y + 60)
                 mainSyncLabel.textColor = UIColor(named: "textColor")
                 mainSyncLabel.alpha = 0
-                mainSyncLabel.text = NSLocalizedString("Sync", comment: "")
+                mainSyncLabel.text = NSLocalizedString("Getting started", comment: "")
                 mainSyncLabel.textAlignment = .center
                 mainSyncLabel.font = UIFont(name: "ProximaNova-Light", size: 25)
                 window.addSubview(mainSyncLabel)
@@ -138,8 +129,7 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
             self.present(alert, animated: true)
         }
         
-        //config collection view
-        collectionView.alpha = 1.0
+
         nextBtn.alpha = 1.0
         mainLabel.alpha = 1.0
         subLabel.alpha = 1.0
@@ -155,87 +145,7 @@ class OnboardingViewController: UIViewController, UICollectionViewDelegate, UICo
         let mainVC = storyboard.instantiateViewController(withIdentifier: "mainVC") as! UITabBarController
         UIApplication.shared.windows.first?.rootViewController = mainVC
         UIApplication.shared.windows.first?.makeKeyAndVisible()
-//        mainVC.modalTransitionStyle = .crossDissolve
-//        mainVC.modalPresentationStyle = .fullScreen
-//        mainVC.modalTransitionStyle = .coverVertical
         self.show(mainVC, sender: .none)
-    }
-    
-    
-//MARK: - Collection View
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sourceImages.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let cell = collectionView.cellForItem(at: indexPath)
-
-        print("selected \([indexPath.row])")
-        
-        //count selected items
-        currentlySelected += 1
-//        print("currently selected: \(currentlySelected)")
-        
-        //set btn enabled
-        nextBtn.isEnabled = true
-        nextBtn.setTitleColor(UIColor(named: "primaryColor"), for: .normal)
-        
-        //set Source active in Coredata
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let userEntity = NSEntityDescription.entity(forEntityName: "ActiveSources", in: managedContext)!
-        
-        let user = NSManagedObject(entity: userEntity, insertInto: managedContext)
-        
-        user.setValue(sources[indexPath.row], forKeyPath: "isActiveSource")
-        
-        do {
-            try managedContext.save()
-            
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
-        if let selectedItems = collectionView.indexPathsForSelectedItems {
-            if selectedItems.contains(indexPath) {
-                collectionView.deselectItem(at: indexPath, animated: true)
-                
-                //count selected items
-                currentlySelected -= 1
-//                print("de-selected \([indexPath.row])")
-                
-                //check if any items are selected and disable if 0
-                if currentlySelected == 0 {
-                    nextBtn.isEnabled = false
-                    nextBtn.setTitleColor(UIColor(named: "lineColor"), for: .normal)
-                }
-//                print("currently selected: \(currentlySelected)")
-                return false
-            }
-        }
-        return true
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SourceCell", for: indexPath) as! SourceCollectionViewCell
-    
-        //cell roundiness
-        cell.sourceImage.layer.cornerRadius = 10.0
-        
-        //cell shadow
-        cell.layer.shadowColor = UIColor.black.cgColor
-        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
-        cell.layer.shadowRadius = 1
-        cell.layer.shadowOpacity = 0.6
-        cell.layer.masksToBounds = false
-        cell.layer.backgroundColor = UIColor.clear.cgColor
-        cell.sourceImage.image = sourceImages[indexPath.row]
-        
-        return cell
     }
     
     
